614f95dd64448885ccd83376091a746fb5f37842
Atharva-Trivedi <atharva.trivedi@walmart.com>
2022-11-16 10:41:03 +0530

    Changes to backend and inclusion of frontend react library

--- EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package com.fullstack.EmployeeManagement.employee;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class EmployeeController
     @Autowired
     private EmployeeService employeeService = new EmployeeService();
 
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees()
     {
         return employeeService.findAllEmployees();
