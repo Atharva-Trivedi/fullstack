33d823d853f1d12d69c9b0b66163ebd2bc0b19e3
Atharva-Trivedi <atharva.trivedi@walmart.com>
2022-12-08 13:17:43 +0530

    update employee Rest Api added

--- EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,15 +37,16 @@ public class EmployeeController
         employeeService.addEmployee(employee);
     }
 
-    @RequestMapping(value = "employees/{id}",method = RequestMethod.DELETE )
+    @RequestMapping(value = "/employees/{id}",method = RequestMethod.DELETE )
     public void deleteEmployee(@PathVariable Long id)
     {
         employeeService.deleteEmployee(id) ;
     }
 
-    @RequestMapping(value = "employees/{id}",method = RequestMethod.PUT)
+    @RequestMapping(value = "/employees",method = RequestMethod.PUT)
     public void updateEmployee(@RequestBody Employee employee)
     {
+        System.out.println("test");
         employeeService.updateEmployee(employee);
     }
 }
--- EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeRepository.java
+++ EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeRepository.java
@@ -3,7 +3,6 @@ package com.fullstack.EmployeeManagement.employee;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
