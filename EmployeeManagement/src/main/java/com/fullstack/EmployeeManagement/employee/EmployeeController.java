cdc57747f6c0959a97bc3fc838cb36c5424afa6b
Atharva-Trivedi <atharva.trivedi@walmart.com>
2022-12-21 18:05:05 +0530

    final touches

--- EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/fullstack/EmployeeManagement/employee/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.fullstack.EmployeeManagement.employee;
 
 import java.util.List;
-// import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -49,7 +48,6 @@ public class EmployeeController
     public void updateEmployee(@RequestBody Employee employee)
     {  
         System.out.println(123);
-        // employee.setId(id);
         System.out.println(employee);
         employeeService.updateEmployee(employee);
     }
